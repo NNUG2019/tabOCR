d4ab0aed2f16c3f094d23c888c7f69615b014bef
dodikus <45990934+dodikus@users.noreply.github.com>
2019-11-29 21:22:19 +0100

    Add files via upload

--- example_generator/bare_bones_imager.py
+++ example_generator/bare_bones_imager.py
@@ -1,4 +1,5 @@
 from openpyxl import Workbook
+from openpyxl.styles import Border, Side
 import excel2img as e2i
 import os
 
@@ -7,12 +8,15 @@ def bare_bones_imager(lst, path):
     book = Workbook()
     sheet = book.active
     counter = 0
+    red = Side(border_style="thin", color="FFFFFF")
+    black = Side(border_style="thin", color="FFFFFF")
     for f in lst:
         i = 0
         for l in f:
             j = 0
             for w in l:
                 sheet.cell(row=i+1, column=j+1).value = w
+                sheet.cell(row=i+1, column=j+1).border = Border(top=red, left=red, bottom=black, right=black)
                 j += 1
             i += 1
 
